c500d85627b87b2efed815963dae10711c512262
stef <sstefstefv@gmail.com>
2019-11-29 21:11:38 +0200

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,7 +8,8 @@ export const addExpense = expense => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = "",
       note = "",
@@ -23,7 +24,7 @@ export const startAddExpense = (expenseData = {}) => {
     };
 
     return database
-      .ref("/expenses")
+      .ref(`users/${uid.uid}/expenses`)
       .push(expense)
       .then(ref => {
         dispatch(
@@ -42,9 +43,10 @@ export const setExpenses = expenses => ({
 });
 
 export const startSetExpenses = () => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref("/expenses")
+      .ref(`users/${uid.uid}/expenses`)
       .once("value")
       .then(snapshot => {
         const expenses = [];
@@ -66,13 +68,13 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 export const startRemoveExpense = ({ id }) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref(`/expenses/${id}`)
+      .ref(`users/${uid.uid}/expenses/${id}`)
       .remove()
       .then(() => {
         dispatch(removeExpense({ id }));
-        console.log("Success");
       });
   };
 };
@@ -85,9 +87,10 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-  return dispatch => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     return database
-      .ref(`/expenses/${id}`)
+      .ref(`users/${uid.uid}/expenses/${id}`)
       .update(updates)
       .then(() => {
         dispatch(editExpense(id, updates));
