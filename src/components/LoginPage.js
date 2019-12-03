0803c4594ba9f2bc08bf118c012f877f3f32b4b2
stef <sstefstefv@gmail.com>
2019-12-03 20:51:31 +0200

    Final styled version of the app

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,16 @@ import { startLogin } from "../actions/auth";
 
 const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Log In</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1>Expensify App</h1>
+        <p className="box-layout__title">
+          It's time to get your expenses under control
+        </p>
+        <button className="button" onClick={startLogin}>
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 };
